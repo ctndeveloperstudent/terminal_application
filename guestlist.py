4a793d07fbd8925cf8296b1934ea6763d361f80a
ctndeveloperstudent <15708@coderacademy.edu.au>
2024-07-03 23:02:44 +1000

    added error handling

--- guestlist.py
+++ guestlist.py
@@ -13,45 +13,49 @@ def view_guests():
         for i, guest in enumerate(guests):
             print(f'{i+1}. {guest}')
 
-def delete_guests():
+def delete_guest():
     if len(guests) == 0:
         print("No guests to delete.")
     else:
         print('Guests:')
         for i, guest in enumerate(guests):
             print(f'{i+1}. {guest}')
-        choice = int(input("Enter the guest number to delete:"))
-
-        if 0 < choice <= len(guests):
-            del guests[choice-1]
-            print("Guest deleted successfuly.")
-        else:
-            print("Invalid guest number.")
+        try:
+            choice = int(input("Enter the guest number to delete: "))
+            if 0 < choice <= len(guests):
+                del guests[choice-1]
+                print("Guest deleted successfully.")
+            else:
+                print("Invalid guest number.")
+        except ValueError:
+            print("Please enter a valid number.")
 
 def main():
-
     while True:
         print("\n===== Event Planner Application =====")
         print("============ Guest List =============")
-        print("1.Add New Guest")
-        print("2.View Guest List")
-        print("3.Delete Guest")
-        print("4.Back to Main Menu")
-        print("5.Quit")
+        print("1. Add New Guest")
+        print("2. View Guest List")
+        print("3. Delete Guest")
+        print("4. Back to Main Menu")
+        print("5. Quit")
 
-        choice = int(input("Enter the number of your choice:"))
-        if choice == 1:
-            add_guest()
-        elif choice == 2:
-            view_guests()
-        elif choice == 3:
-            delete_guests()
-        elif choice == 4:
-            pass
-        elif choice == 5:
-            print("Thank you for using the Event Planner Application.")
-            break
-        else:
-            print("Invalid choice. Please try again")
+        try:
+            choice = int(input("Enter the number of your choice: "))
+            if choice == 1:
+                add_guest()
+            elif choice == 2:
+                view_guests()
+            elif choice == 3:
+                delete_guest()
+            elif choice == 4:
+                continue
+            elif choice == 5:
+                print("Thank you for using the Event Planner Application.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+        except ValueError:
+            print("Please enter a valid number.")
 
 main()
--- todolist.py
+++ todolist.py
@@ -7,55 +7,55 @@ def add_task():
 
 def view_tasks():
     if len(tasks) == 0:
-        print("No tasks on the to do list.")
+        print("No tasks on the to-do list.")
     else:
         print("List of tasks:")
         for i, task in enumerate(tasks):
             print(f'{i+1}. {task}')
 
-def delete_tasks():
+def delete_task():
     if len(tasks) == 0:
         print("No tasks to delete.")
     else:
         print('Tasks:')
         for i, task in enumerate(tasks):
             print(f'{i+1}. {task}')
-        choice = int(input("Enter the task number to delete:"))
-
-        if 0 < choice <= len(tasks):
-            del tasks[choice-1]
-            print("Task deleted successfuly.")
-        else:
-            print("Invalid task number.")
+        try:
+            choice = int(input("Enter the task number to delete: "))
+            if 0 < choice <= len(tasks):
+                del tasks[choice-1]
+                print("Task deleted successfully.")
+            else:
+                print("Invalid task number.")
+        except ValueError:
+            print("Please enter a valid number.")
 
 def main():
-
     while True:
         print("\n===== Event Planner Application =====")
-        print("\n============ To Do List =============")
-        print("1.Add Task")
-        print("2.View Task")
-        print("3.Delete Task")
-        print("4.Back to Main Menu")
-        print("5.Quit")
+        print("============ To-Do List =============")
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Delete Task")
+        print("4. Back to Main Menu")
+        print("5. Quit")
 
-        choice = int(input("Enter the number of your choice:"))
-        if choice == 1:
-            add_task()
-        elif choice == 2:
-            view_tasks()
-        elif choice == 3:
-            delete_tasks()
-        elif choice == 4:
-            pass
-        elif choice == 5:
-            print("Thank you for using the Event Planner Application.")
-            break
-        else:
-            print("Invalid choice. Please try again.")
+        try:
+            choice = int(input("Enter the number of your choice: "))
+            if choice == 1:
+                add_task()
+            elif choice == 2:
+                view_tasks()
+            elif choice == 3:
+                delete_task()
+            elif choice == 4:
+                continue
+            elif choice == 5:
+                print("Thank you for using the Event Planner Application.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+        except ValueError:
+            print("Please enter a valid number.")
 
 main()
-
-    
-
-
